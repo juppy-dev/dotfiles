1095e768eebaddf6f70b34f8c5fc02377867d30c
Paolo Arroyo <paolo@onearrow.io>
2026-07-08 14:21:09 +0800

    feat(dotfiles): track shell, git, editor, cli, and ssh configs

---
+++ dot_config/xplr/init.lua
@@ -0,0 +1,33 @@
+version = '1.1.0'
+
+local home = os.getenv("HOME")
+local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
+local xpm_url = "https://github.com/dtomvan/xpm.xplr"
+
+package.path = package.path
+  .. ";"
+  .. xpm_path
+  .. "/?.lua;"
+  .. xpm_path
+  .. "/?/init.lua"
+
+os.execute(
+  string.format(
+    "[ -e '%s' ] || git clone '%s' '%s'",
+    xpm_path,
+    xpm_url,
+    xpm_path
+  )
+)
+require('xpm').setup {
+  'dtomvan/xpm.xplr',
+  { 'dtomvan/extra-icons.xplr',
+      after = function()
+          xplr.config.general.table.row.cols[2] = { format = "custom.icons_dtomvan_col_1" }
+      end
+  },
+  'dy-sh/dysh-style.xplr',
+  {
+    'github.com:dy-sh/dysh-style.xplr'
+  }
+}
